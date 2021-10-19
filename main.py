8ee7f251c5e3e7e5ca6a4c060e7dec95c63da3a1
sm354 <shubhamiitd18@gmail.com>
2021-10-19 19:50:43 +0530

    part-2 0.11, 0.24 on bolt, blurcar2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ipdb # remove if needed
 from tqdm import tqdm # remove if needed
+import subprocess # remove if needed
 import os
 import cv2
 import argparse
@@ -16,7 +17,7 @@ def appearence_based_tracking(args):
         image = cv2.imread(os.path.join(os.path.join(args.data_dir, "img"), img_name), 0)
         image = cv2.GaussianBlur(image,(3,3),0)
 
-        if(args.technique == 'ncc'):
+        if(args.m1_method == 'ncc'):
             result = cv2.matchTemplate(image,template,cv2.TM_CCOEFF_NORMED)
             cv2.normalize(result,result,cv2.NORM_MINMAX)
             _,_,_,maxLoc = cv2.minMaxLoc(result)
@@ -29,10 +30,12 @@ def appearence_based_tracking(args):
 
         pred_w = template_coord[2]  
         pred_h = template_coord[3] 
+        
         # predicted tuple
-        predictions.append(str(pred_x)  + ',' + str(pred_y)  + ',' + str(pred_w) + ',' + str(pred_h))
+        predicted_bb = [pred_x, pred_y, pred_w, pred_h]
+        predictions.append(",".join(map(str, predicted_bb)))
 
-    save_predictions(predictions, name = args.method)
+    save_predictions(predictions, args)
 
 def lucas_kanade(args):
     # predictions is a list containing strings of bounding boxes | right now it just contains the template coordinates
@@ -44,75 +47,29 @@ def lucas_kanade(args):
 
         # preprocessing on the current frame
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # is it really needed from implementation perspective?
-        image = cv2.GaussianBlur(image, (3,3)) # @aditi: check this hyperparams; we may remove this if useless 
+        image = cv2.GaussianBlur(image, (3,3),0) # @aditi: check this hyperparams; we may remove this if useless 
         # further pre-processing required ? like normalization ??
-        
+
+        # image, template, template_img all are gray-scale
         warp_params = run_LK_algo(frame=image, template=template, template_coord=template_coord)
         W = get_Warp(warp_params=warp_params, affine=True)
 
         # predict the template_coord using warp_params | this is geometric transformation
-        point_1 = np.array(template_coord[0:2]).reshape(2,1)
-        point_1 = np.dot(W, point_1).astype(np.int)
+        point_1 = np.array(template_coord[0:2] + [1]).reshape(3,1)
+        point_1 = np.dot(W, point_1).astype(np.int).reshape(-1)
         x2, y2 = template_coord[0] + template_coord[2], template_coord[1] + template_coord[3]
-        point_2 = np.array([x2, y2]).reshape(2,1)
-        point_2 = np.dot(W, point_2).astype(np.int)
+        point_2 = np.array([x2, y2] + [1]).reshape(3,1)
+        point_2 = np.dot(W, point_2).astype(np.int).reshape(-1)
 
-        predicted_bb = list(point_1) + list(point_2 - point_1)
-        predictions.append(",".join(predicted_bb))
+        predicted_bb = point_1.tolist() + (point_2 - point_1).tolist()
+        predictions.append(",".join(map(str, predicted_bb)))
 
         # update the template, template_img, and template_coord
-        template_coord[0], template_coord[1], template_coord[2], template_coord[3] = point_1[0], point_1[1], point_2[0] - point_1[0], point_2[1] - point_1[1]
-        template_img = image
-        template = get_patch(template_img, template_coord)
+        # template_coord[0], template_coord[1], template_coord[2], template_coord[3] = point_1[0], point_1[1], point_2[0] - point_1[0], point_2[1] - point_1[1]
+        # template_img = image
+        # template = get_patch(template_img, template_coord)
 
-    save_predictions(predictions, name = args.method)
-
-def run_LK_algo(frame, template, template_coord, iterations=100):
-    (h, w) = frame.shape
-
-    # initialize warp_params such that W = get_Warp(warp_params) is Identity matrix
-    warp_params = np.zeros(6)
-
-    for i in range(iterations):
-        # 1. warp the frame with W(warp_params)
-        W = get_Warp(warp_params=warp_params, affine=True)
-        warped_frame = cv2.warpAffine(src=frame, M=W, dsize=(frame.shape[1], frame.shape[0]), flags=cv2.WARP_INVERSE_MAP) # flags = cv2.INTER_CUBIC # adding warp_inv_map as it should be IG
-        warped_patch = get_patch(frame, template_coord, gray=True).astype(np.int)
-
-        # 2. Get the error between T(x) and I(W(x;p))
-        assert template.dtype == warp_params.dtype
-        error = template - warped_patch
-
-        # 3. compute warped gradients Del I, evaluated at W(x;p)
-        # Reference: https://opencv24-python-tutorials.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_gradients/py_gradients.html
-        sobel_x, sobel_y = cv2.Sobel(frame, cv2.CV_64F, 1, 0, ksize=5), cv2.Sobel(frame, cv2.CV_64F, 0, 1, ksize=5) # find gradients
-        sobel_x, sobel_y = cv2.warpAffine(sobel_x, W, (w, h)), cv2.warpAffine(sobel_y, W, (w, h)) # warp the gradient-image
-        sobel_x, sobel_y = get_patch(sobel_x, template_coord), get_patch(sobel_y, template_coord) # get the warped-gradient-patch
-
-        # 4. evaluate the jacobian of the warping
-
-        # 5. Compute the steepest descent using Jacobian and Image(frame) gradient
-
-        # 6. Compute Inverse Hessian
-
-        # 7. Multiply Steepest descent with the error
-
-        # 8. Compute del_warp_params 
-
-        # 9. update warp_params
-
-        # 10. repeat until del_warp_params < epsilon
-
-    pass
-
-def get_Warp(warp_params, affine=True):
-    W = None
-    if affine:
-        W = [1+warp_params[0], warp_params[2], warp_params[4], warp_params[1], 1+warp_params[3], warp_params[5]] # de-plag
-        W = np.array(W)
-        W = W.reshape(2,3)
-    
-    return W
+    save_predictions(predictions, args)
 
 def pyramid_lk(args):
     pass
@@ -129,3 +86,7 @@ if __name__ == "__main__":
 
     print("using %s for template tracking"%(FUNCTION_MAPPER[args.method]))
     FUNCTION_MAPPER[args.method](args)
+
+    if args.eval:
+        print("evaluating...")
+        subprocess.call("python eval.py -g %s/groundtruth_rect.txt -p %s/predictions_part%s.txt"%(args.data_dir, args.data_dir, args.method), shell=True)
--- utils.py
+++ utils.py
@@ -12,13 +12,120 @@ def parse_args():
         help="Path to the dataset")
     parser.add_argument('-m', '--method', type=int, default=3, \
         help="method of template tracking: 1-appearence, 2-LK, 3-PyramidLK")
-    parser.add_argument('-t', '--technique',type = str,default = 'ncc' , help = "technique for appearance based tracking")
+    parser.add_argument('-m1', '--m1_method',type = str,default = 'ncc' , help = "method for appearance based tracking")
+    parser.add_argument('-e', '--eval', action="store_true", help="evaluate or not")
     args = parser.parse_args()
     return args
+
+def run_LK_algo(frame, template, template_coord, iterations=100, epsilon=0.001):
+    # frame and template are gray scale
+    (h, w) = frame.shape
+
+    # initialize warp_params such that W = get_Warp(warp_params) is Identity matrix
+    warp_params = np.zeros(6)
+
+    for i in range(iterations):
+        # 1. warp the frame with W(warp_params)
+        W = get_Warp(warp_params=warp_params, affine=True)
+        warped_frame = cv2.warpAffine(src=frame, M=W, dsize=(frame.shape[1], frame.shape[0]), flags=cv2.WARP_INVERSE_MAP) # flags = cv2.INTER_CUBIC # adding warp_inv_map as it should be IG
+        warped_patch = get_patch(frame, template_coord, gray=True).astype(np.uint8)
+
+        # 2. Get the error between T(x) and I(W(x;p))
+        assert template.dtype == warped_patch.dtype
+        error = template - warped_patch
+
+        # 3. compute warped gradients Del I, evaluated at W(x;p)
+        # Reference: https://opencv24-python-tutorials.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_gradients/py_gradients.html
+        sobel_x, sobel_y = cv2.Sobel(frame, cv2.CV_64F, 1, 0, ksize=5), cv2.Sobel(frame, cv2.CV_64F, 0, 1, ksize=5) # find gradients
+        sobel_x, sobel_y = cv2.warpAffine(sobel_x, W, (w, h)), cv2.warpAffine(sobel_y, W, (w, h) ) # warp the gradient-image
+        sobel_x, sobel_y = get_patch(sobel_x, template_coord), get_patch(sobel_y, template_coord) # get the warped-gradient-patch
+        del_I = np.expand_dims(np.stack([sobel_x, sobel_y], axis=2), axis=2)
+
+        # 4. evaluate the jacobian of the warping
+        warp_jacobian = get_warp_jacobian(shape=template.shape, transformation=1) # (template.shape,2,6)
+
+        # 5. Compute the steepest descent using Jacobian and Image(frame) gradient
+        stp_dsc = np.matmul(del_I, warp_jacobian) # (template.shape,1,6)
+
+        # 6. Compute Inverse Hessian
+        hessian = np.matmul(stp_dsc.transpose(0,1,3,2), stp_dsc) # (template.shape,6,6)
+        hessian = np.sum(hessian, axis=(0,1))
+        assert hessian.shape == (6,6)
+        H_inv = np.linalg.pinv(hessian)
+
+        # 7. Multiply Steepest descent with the error
+        error = np.expand_dims(np.expand_dims(error, axis=2), axis=2)
+        # intermediate = np.matmul(stp_dsc.transpose(0,1,3,2), error) # (template.shape,6,1)
+        intermediate = stp_dsc.transpose(0,1,3,2) * error
+        intermediate = np.sum(intermediate, axis=(0,1))
+
+        # 8. Compute del_warp_params 
+        del_warp_params = np.matmul(H_inv, intermediate).reshape(-1)
+
+        # 9. update warp_params
+        warp_params += del_warp_params
+
+        # 10. repeat until del_warp_params < epsilon
+        if np.linalg.norm(del_warp_params) < epsilon:
+            # print("norm of del_warp_params less than epsilon, and equal to", np.linalg.norm(del_warp_params))
+            break
+
+    return warp_params
 
 def get_patch(image, bb, gray = True):
     x, y, w, h = bb[0], bb[1], bb[2], bb[3] # w is along x (right), h is along y (down)
     return image[y:y+h, x:x+w] if gray == True else image[y:y+h, x:x+w, :]
+
+def get_Warp(warp_params, affine=True):
+    W = None
+    if affine:
+        W = [1+warp_params[0], warp_params[2], warp_params[4], warp_params[1], 1+warp_params[3], warp_params[5]] # de-plag
+        W = np.array(W)
+        W = W.reshape(2,3)
+    return W
+
+def get_warp_jacobian(shape, transformation = 1):
+    jacobian = None
+    (h, w) = shape
+
+    ones_patch = np.ones(shape)
+    zeros_patch = np.zeros(shape)
+    img_patch = np.meshgrid(np.arange(0,w), np.arange(0,h))
+
+    # since we will be using matrix multiplication where matrices are 4-D and contain elements as matrices itself
+    # following np.matmul documentation 
+
+    if transformation == 0:
+        # translation transformation
+        # grad_W = [
+        #     [1, 0],
+        #     [0, 1]
+        # ]
+        jacobian_x = np.stack([ones_patch, zeros_patch], axis=2)
+        jacobian_y = np.stack([zeros_patch, ones_patch], axis=2)
+        jacobian = np.stack([jacobian_x, jacobian_y], axis=2)
+        assert len(jacobian.shape) == 4 and jacobian.shape[-2:] == (2,2)
+
+    elif transformation == 1:
+        # affine transformation
+        # grad_W = [
+        #     [x, 0, y, 0, 1, 0],
+        #     [0, x, 0, y, 0, 1]
+        # ]
+        jacobian_x = np.stack([img_patch[0], zeros_patch, img_patch[1], zeros_patch, ones_patch, zeros_patch], axis=2)
+        jacobian_y = np.stack([zeros_patch, img_patch[0], zeros_patch, img_patch[1], zeros_patch, ones_patch], axis=2)
+        jacobian = np.stack([jacobian_x, jacobian_y], axis=2)
+        assert len(jacobian.shape) == 4 and jacobian.shape[-2:] == (2,6)
+
+    elif transformation == 2:
+        # projective tranformation
+        # grad_W = [
+        #     [],
+        #     []
+        # ]
+        pass
+
+    return jacobian
 
 def read_dataset(args):
     # predictions of bounding boxes
@@ -30,7 +137,7 @@ def read_dataset(args):
     
     # ground truth - only for getting template
     ground_truth = open(os.path.join(args.data_dir, "groundtruth_rect.txt"), 'r').readlines()
-    template_coord = ground_truth[0][:-1].split(",") # (x,y,w,h)
+    template_coord = ground_truth[0][:-1].split(",") # [x,y,w,h]
     # str to int
     for i in range(4):
         template_coord[i] = int(template_coord[i])
@@ -43,10 +150,11 @@ def read_dataset(args):
 
     return predictions, frame_names, template_img, template_coord
 
-def save_predictions(predictions, name):
+def save_predictions(predictions, args):
     # input a list of tuples
     predictions = "\n".join(predictions)+"\n"
-    write_file = open("predictions_part%s.txt"%(str(name)), "w")
+    
+    write_file = open(os.path.join(args.data_dir, "predictions_part%s.txt"%(str(args.method))), 'w')
     write_file.writelines(predictions)
     print("predictions saved...!")
 
